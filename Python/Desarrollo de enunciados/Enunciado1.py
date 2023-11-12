66378016a5601117224a8fa69f4a11e4bb9b420b
ArturoLucero28 <146435794+ArturoLucero28@users.noreply.github.com>
2023-11-12 11:34:15 +0100

    Cambio

---
+++ Python/Desarrollo de enunciados/Enunciado1.py
@@ -0,0 +1,22 @@
+def enunciado1():
+    #Creamos una variable en modo lista
+    notas = []
+    print("Ingresa las calificaciones de los 5 módulos:")
+    #un bucle de rango 5 al pedirnos 5 modulos
+    for i in range(5):
+        modulo = float(input(f"Ingresa una nota: "))
+        notas.append(modulo)
+        i= i +1
+    #Buscamos el mayor,menos y suma de la lista
+    mayor = max(notas)
+    menor = min(notas)
+    suma = sum(notas)
+    #Y para la media la conseguimos haciendo la suma de todos diviendolo entre todos los modulos
+    media = suma / 5
+
+    print(f"Calificación mayor: {mayor}")
+    print(f"Calificación menor: {menor}")
+    print(f"Media de calificaciones: {media}")
+
+enunciado1()
+
