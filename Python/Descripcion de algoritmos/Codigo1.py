4ac2589304f11fb59b762580b762bee7845153a7
ArturoLucero28 <146435794+ArturoLucero28@users.noreply.github.com>
2023-11-12 11:33:01 +0100

    Cambio

---
+++ Python/Descripcion de algoritmos/Codigo1.py
@@ -0,0 +1,27 @@
+#Realiza un script en python que compare 3 números 
+#insertados por el usuario, en un bucle donde nos de 
+#el mayor de los 3 hasta que el usuario ingrese el 0 y finalize
+
+def un_proceso():
+    a, b, c, final = 0, 0, 0, 1
+
+    while final != 0:
+        print("Escribe tres números distintos, por favor")
+        a = int(input("Ingrese el primer numero: "))
+        b = int(input("Ingrese el segundo numero: "))
+        c = int(input("Ingrese el tercer numero: "))
+        if a > b:
+            if a > c:
+                print("a es el mayor:", a)
+            else:
+                print("c es el mayor:", c)
+        else:
+            if b > c:
+                print("b es el mayor:", b)
+            else:
+                print("c es el mayor:", c)
+
+        print("¿Quiere finalizar el proceso? Teclee 0 para salir.")
+        final = int(input())
+
+un_proceso()
